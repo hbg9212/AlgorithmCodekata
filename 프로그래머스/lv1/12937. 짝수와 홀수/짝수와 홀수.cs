d64bda05ea7acbf1d5548fe53dbd53f92310892b
한병권 <50606825+hbg9212@users.noreply.github.com>
2023-08-24 11:11:54 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.21 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.cs
@@ -0,0 +1,10 @@
+
+    public class Solution
+    {
+        public string solution(int num)
+        {
+            string answer = num % 2 == 0 ? "Even" : "Odd";
+            return answer;
+        }
+    }
+
