dd68f32a5f300c772d82f0871d950a2419dc8248
한병권 <50606825+hbg9212@users.noreply.github.com>
2023-08-24 14:29:34 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.41 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public string solution(string phone_number) {
+        string answer = "";
+        
+        for(int i = 0; i < phone_number.Length; i++)
+        {
+            if( i < phone_number.Length - 4)
+            {
+                answer = answer + "*";
+            }
+            else
+            {
+                answer = answer + phone_number.Substring(i,1);
+            }
+        }
+        
+        return answer;
+    }
+}
